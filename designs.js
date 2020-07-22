77557c3f941ebe9214be360afa6c44c562601eee
segalbny <68568129+segalbny@users.noreply.github.com>
2020-07-22 18:16:32 -0400

    click fixes
    fixes

--- designs.js
+++ designs.js
@@ -1,35 +1,34 @@
 // Select color input
 // Select size input
-var height, width, color;
 // When size is submitted by the user, call makeGrid()
-$('#sizePicker').submit(function (event) {
-   event.preventDefault();  
-   height = $('#inputHeight').val();
-   width =  $('#inputWidth').val();
-   makeGrid(height, width)
-   console.log(`Height: ${height}add width: ${width}`);
 
-})
+var colorPicker = document.getElementById ('colorPicker').value;
+var sizePicker = document.getElementById ('sizePicker').value;
 
-function makeGrid(n,m) {
-    $('tr').remove();
+sizePicker.addEventListener("submit", function(event){
+    event.preventDefault();
+   
+function addColor(event) {
+    var color = document.getElementById ('colorPicker').value;
+        event.target.style.backgroundColor = color;
+        event.preventDefault();
+      };
+      grid.addEventListener("click", addColor);
+      grid.innerHTML ="" ;
 
-// Your code goes here!
-   for (var d = 1; d <= n; d++) {
-       $('#pixelCanvas').append('<tr id="table' + d + '"></tr>')
-   for (var e = 1; e <=m; e++) { 
-        $('#table'+ d).append('<td></td>')
-   }
-}
-$('td').click(function addColor() {
-  color = $('#colorPicker').val();
-  
-  if  ($(this).attr('style')){
-      $(this).removeAttr('style')
-    } else {
-      $(this).attr('style', 'background-color:'+ color)
-    }
-  })
-}
+function submitButton() {
+    event.preventDefault()
+    var height = document.getElementById ('inputWidth').value
+    var width = document.getElementById ('inputHeight').value
+    var table = document.getElementById("pixelCanvas") 
+    makeGrid(height, width)
 
-
+function makeGrid(height, width) {
+    for (let m = 0; m < height;r++) {
+        let row = document.createElement ("tr");  }  
+        for (let n = 0; n < width; w++) {
+            var cell = document.createElement ("td");
+            insertRow.appendChild (Cell); 
+            cell.addEventListener('click', function (colorclick)  {
+                cell.style.backgroundColor = color.value;
+        } 
