76809711cd860acbff815b7a81d28dbb1343c045
segalbny <68568129+segalbny@users.noreply.github.com>
2020-07-23 09:44:41 -0400

    updated code fix
    working on fix

--- designs.js
+++ designs.js
@@ -1,3 +1,6 @@
+// Select color input
+// Select size input
+
 var colorPicker = document.getElementById ('colorPicker');
 var setColor =colorPicker.value;
 var sizePicker = document.getElementById ('sizePicker');
@@ -6,24 +9,30 @@ colorPicker.addEventListener('change', function () {
     setColor = document.getElementById ('colorPicker').value;
 });
 
+// When size is submitted by the user, call makeGrid()
+
 sizePicker.addEventListener('submit', function () {
     event.preventDefault()
-    var height = document.getElementById ('inputWidth').value
-    var width = document.getElementById ('inputHeight').value
+    var height = document.getElementById ('inputHeight').value
+    var width = document.getElementById ('inputWidth').value
     makeGrid(height, width)
 });
+
+// Your code goes here!
+
 function makeGrid(height, width) {
     var table = document.getElementById('pixelCanvas')
     table.innerHTML = '';
-    
-    for (var m = 0; m < height; m++) {
-        var row = document.createElement('tr');  
-        
-        for (var n = 0; n < width; n++) {
-            var cell = document.createElement ('td');
-            row.appendChild (cell); 
 
-            cell.addEventListener('click', function ()  {
+    for (var m= 0; m <= width; m++) {
+        var row = document.createElement('tr');  
+        row.appendChild (column)
+        
+        for (var n = 0; n <= height; n++) {
+            var column = document.createElement ('td');
+            column.appendChild (row); 
+
+            column.addEventListener('click', function ()  {
                if (this.style.backgroundColor) {
                 this.style.backgroundColor = ''
                }  else  { 
